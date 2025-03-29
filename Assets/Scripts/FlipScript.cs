0af7730f9e434ee39caf496a1418f3a77426aa09
Mantelgen <84387756+Mantelgen@users.noreply.github.com>
2025-03-29 23:22:17 +0200

    FirstDay
    The end of first day

--- Assets/Scripts/FlipScript.cs
+++ Assets/Scripts/FlipScript.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FlipScript : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     private AudioSource audioSource;
+
+    [SerializeField]
+    private TextMeshPro score;
+    [SerializeField]
+    private TextMeshPro answer;
 
     private string number;
 
@@ -24,6 +30,8 @@ public class FlipScript : MonoBehaviour
         facedUp = false;
         number = gameObject.name;
         number=number.Split(' ')[1];
+        score.enabled = false;
+        answer.enabled = false;
     }
 
  
@@ -37,10 +45,19 @@ public class FlipScript : MonoBehaviour
             for (float i = 0f; i <= 180f; i += 5f)
             {
                 transform.rotation = Quaternion.Euler(i, 0, 0);
+                if (i == 70f)
+                {
+
+                    answer.enabled = true;
+                    answer.ForceMeshUpdate();
+                    score.enabled = true;
+
+
+                }
+
                 if (i == 80f)
                 {
                     spriteRenderer.sprite = faceSprite;
-                    
                 }
                 yield return new WaitForSeconds(0.01f);
             }
@@ -52,7 +69,7 @@ public class FlipScript : MonoBehaviour
         if(Input.GetKeyDown(number) && coroutineAllowed)
         {
                 StartCoroutine(Flip());
-            
+     
         }
     }
 }
